99a824802f35bff1985ade3d23de10a432ce3352
James Paterson <j@presstube.com>
2016-05-27 17:01:05 -0400

    tidy

--- src/components/app.js
+++ src/components/app.js
@@ -6,33 +6,8 @@ import FullscreenCJSUnit from "./fullscreen-cjs-unit"
 export default class App extends React.Component {
   render () {
     return <FullscreenCJSUnit name="aunit" />
+    // return <FullscreenCJSUnit name="bunit" />
+    // return <FullscreenCJSUnit name="cunit" />
   }
 }
 
-/*
-
-      <FullscreenCJSUnit
-        filename="a-unit.js"
-        namespace="aunit"
-        width={800}
-        height={800}
-        fps={10}
-      />
-
-      <FullscreenCJSUnit
-        filename="b-unit.js"
-        namespace="bunit"
-        width={800}
-        height={500}
-        fps={20}
-      />
-
-      <FullscreenCJSUnit
-        filename="c-unit.js"
-        namespace="cunit"
-        width="700"
-        height="900"
-        fps={30}
-      />
-
-*/
